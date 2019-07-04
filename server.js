6fe9031149b8f3b2796c1292c14124c4e2e3c331
unknown <pwhynotfilms@gmail.com>
2019-07-04 01:25:35 -0500

    index.html replaced by handlebars

--- server.js
+++ server.js
@@ -1,32 +1,24 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
 const axios = require("axios");
 const cheerio = require("cheerio");
 const logger = require("morgan");
-
+const exphbs = require("express-handlebars");
 
 const db = require("./models");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 app.use(logger("dev"));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
-//app.use(express.static("public"));
-
-app.use(express.static(process.cwd() + "/public"));
-const exphbs = require("express-handlebars");
-app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
-);
-app.set("view engine", "handlebars");
+app.use(express.static("views"));
 
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/reason-scraper";
 
