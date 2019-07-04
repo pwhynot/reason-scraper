f2fb419bbd9574caf233454792aedc32795d42ca
unknown <pwhynotfilms@gmail.com>
2019-07-03 20:45:07 -0500

    inital commit

---
+++ server.js
@@ -0,0 +1,85 @@
+var express = require("express");
+
+var mongoose = require("mongoose");
+var exhbs = require("express-handlebars");
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+
+var db = require("./models");
+
+var PORT = PORT = process.env.PORT || 3000;
+
+var app = express();
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+
+mongoose.connect("mongodb://localhost", { useNewUrlParser: true });
+
+app.get("/scrape", function(req, res) {
+  axios.get("https://reason.com/").then(function(response) {
+    var $ = cheerio.load(response.data);
+
+    $("article h2").each(function(i, element) {
+      var result = {};
+      result.title = $(this)
+        .children("a")
+        .text();
+      result.link = $(this)
+        .children("a")
+        .attr("href");
+
+      db.Article.create(result)
+        .then(function(dbArticle) {
+          console.log(dbArticle);
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
+    });
+    res.send("Scrape Complete");
+  });
+});
+
+app.get("/articles", function(req, res) {
+  db.Article.find({})
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+app.get("/articles/:id", function(req, res) {
+  db.Article.findOne({ _id: req.params.id })
+    .populate("note")
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+app.post("/articles/:id", function(req, res) {
+  db.Note.create(req.body)
+    .then(function(dbNote) {
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    })
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
