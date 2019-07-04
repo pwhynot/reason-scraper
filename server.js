b9674a5d9c69dac491c09363c32f2be5063f94f5
unknown <pwhynotfilms@gmail.com>
2019-07-03 21:09:26 -0500

    routes and models

--- server.js
+++ server.js
@@ -1,85 +1,107 @@
-var express = require("express");
-
-var mongoose = require("mongoose");
-var exhbs = require("express-handlebars");
-var axios = require("axios");
-var cheerio = require("cheerio");
-
-
-var db = require("./models");
-
-var PORT = PORT = process.env.PORT || 3000;
-
-var app = express();
+const express = require("express");
+const mongoose = require("mongoose");
+const exphbs = require("express-handlebars");
+const axios = require("axios");
+const cheerio = require("cheerio");
+const logger = require("morgan");
 
 
+const db = require("./models");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-mongoose.connect("mongodb://localhost", { useNewUrlParser: true });
-
-app.get("/scrape", function(req, res) {
-  axios.get("https://reason.com/").then(function(response) {
-    var $ = cheerio.load(response.data);
-
-    $("article h2").each(function(i, element) {
-      var result = {};
-      result.title = $(this)
-        .children("a")
-        .text();
-      result.link = $(this)
-        .children("a")
-        .attr("href");
-
-      db.Article.create(result)
-        .then(function(dbArticle) {
-          console.log(dbArticle);
-        })
-        .catch(function(err) {
-          console.log(err);
-        });
-    });
-    res.send("Scrape Complete");
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true
   });
-});
 
-app.get("/articles", function(req, res) {
-  db.Article.find({})
-    .then(function(dbArticle) {
-      res.json(dbArticle);
+
+  app.engine("handlebars", exphbs({
+      defaultLayout: "main"
     })
-    .catch(function(err) {
-      res.json(err);
+  );
+
+  app.set("view engine", "handlebars");
+  
+  require("./routes/htmlRoute")(app);
+ 
+  app.get("/scrape", function (req, res) {
+    axios.get("https://reason.com/").then(function (response) {
+      var $ = cheerio.load(response.data);
+      $("article h2").each(function (i, element) {
+        var result = {};
+        result.title = $(this)
+          .children("a")
+          .text();
+        result.link = $(this)
+          .children("a")
+          .attr("href");
+
+        db.Article.create(result)
+          .then( dbArticle =>  {
+            console.log(dbArticle);
+            res.render("index", {this: title});
+          })
+          .catch(function (err) {
+            console.log(err);
+          });
+      });
+      console.log(dbArticle)
     });
-});
 
-app.get("/articles/:id", function(req, res) {
-  db.Article.findOne({ _id: req.params.id })
-    .populate("note")
-    .then(function(dbArticle) {
-      res.json(dbArticle);
-    })
-    .catch(function(err) {
-      res.json(err);
-    });
-});
+  });
 
-app.post("/articles/:id", function(req, res) {
-  db.Note.create(req.body)
-    .then(function(dbNote) {
-      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
-    })
-    .then(function(dbArticle) {
-      res.json(dbArticle);
-    })
-    .catch(function(err) {
-      res.json(err);
-    });
-});
+  app.get("/articles", function (req, res) {
+    db.Article.find({})
+      .then(function (dbArticle) {
+        res.json(dbArticle);
+      })
+      .catch(function (err) {
+        res.json(err);
+      });
+  });
 
-app.listen(PORT, function() {
-  console.log("App running on port " + PORT + "!");
-});
+  app.get("/articles/:id", function (req, res) {
+    db.Article.findOne({
+        _id: req.params.id
+      })
+      .populate("note")
+      .then(function (dbArticle) {
+        res.json(dbArticle);
+      })
+      .catch(function (err) {
+        res.json(err);
+      });
+  });
+  
+  app.post("/articles/:id", function (req, res) {
+    db.Note.create(req.body)
+      .then(function (dbNote) {
+        return db.Article.findOneAndUpdate({
+          _id: req.params.id
+        }, {
+          note: dbNote._id
+        }, {
+          new: true
+        });
+      })
+      .then(function (dbArticle) {
+        res.json(dbArticle);
+      })
+      .catch(function (err) {
+        res.json(err);
+      });
+  });
+
+  app.listen(PORT, function () {
+    console.log("App running on port " + PORT + "!");
+  });
