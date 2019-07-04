1fac12c61e9526b4cb84799c9a47962fb3570af8
unknown <pwhynotfilms@gmail.com>
2019-07-03 23:57:08 -0500

    error fix attempt 4

--- server.js
+++ server.js
@@ -43,12 +43,68 @@ mongoose.connection.once("open", () => console.log("Good to go!"))
         console.warn("Warning", error);
     });
   
-  //require("./routes/htmlRoute")(app);
+  require("./routes/htmlRoute")(app);
 
-  const routes = require("./controllers/scraper");
-  app.use("/", routes);
- 
+  app.get("/scrape", function(req, res) {
+    axios.get("https://reason.com/").then(function(response) {
+      var $ = cheerio.load(response.data);
+      $("p.title").each(function(i, element) {
+        var result = {};
+      result.title = $(this)
+        .children("a")
+        .text();
+      result.link = $(this)
+        .children("a")
+        .attr("href");
+
+      db.Article.create(result)
+        .then( dbArticle =>  {
+          console.log(dbArticle);
+          res.render("index", {this: title});
+        })
+        .catch(function (err) {
+          console.log(err);
+        });
+      });
+
+      res.send("Scrape Complete");
+    });
+  });
+
+  app.get("/articles", function(req, res) {
+    db.Article.find({})
+      .then(function(dbArticle) {
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  });
   
+  app.get("/articles/:id", function(req, res) {
+    db.Article.findOne({ _id: req.params.id })
+      .populate("note")
+      .then(function(dbArticle) {
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  });
+
+  app.post("/articles/:id", function(req, res) {
+    db.Note.create(req.body)
+      .then(function(dbNote) {
+        return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+      })
+      .then(function(dbArticle) {
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  })
+
   app.listen(PORT, function () {
     console.log("App running on port " + PORT + "!");
   });
