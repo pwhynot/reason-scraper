011b1ecb43b95f819bcca35a35f7b37171ce750a
unknown <pwhynotfilms@gmail.com>
2019-07-08 15:22:41 -0500

    error fix attempt

--- server.js
+++ server.js
@@ -18,6 +18,8 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
+app.use(express.static("public"));
+
 app.use(express.static("views"));
 
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
